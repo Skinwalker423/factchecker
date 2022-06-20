22d00274a8141661b412daf3da0ed40b68d57f91
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-06-20 15:23:51 -0700

    adding payment intent

--- src/components/form/donationForm.js
+++ src/components/form/donationForm.js
@@ -1,27 +1,63 @@
 import Button from "../button/button-component";
-import { CardElement } from "@stripe/react-stripe-js";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import './form-styles.css';
 
 
 const DonationForm = () => {
+
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const paymentFormHandler = async (e) => {
+        e.preventDefault();
+
+        if(!stripe || !elements){
+        return;
+        }
+
+        
+
+    }
+
+
+
+    const inputStyle = {
+      iconColor: 'red',
+      color: '#ff0',
+      fontWeight: '500',
+      fontFamily: 'Roboto, Open Sans, Segoe UI, sans-serif',
+      fontSize: '25px',
+      fontSmoothing: 'antialiased',
+      ':-webkit-autofill': {
+        color: '#fce883',
+      },
+      '::placeholder': {
+        color: '#87BBFD',
+      },
+}
+
     return (
         <div className="container donationBody">
         <form className="donationForm">
             <h1>Donation Form</h1>
             <section className="form-group">
                 <label htmlFor="firstName">First Name</label>
-                <input className="form-control" name="firstName" type={'text'} />
+                <input placeholder="John" className="form-control" name="firstName" type={'text'} />
             </section>
             <section className="form-group">
                 <label htmlFor="lastName">Last Name</label>
-                <input className="form-control" name="lastName" type={'text'} />
+                <input placeholder="Doe" className="form-control" name="lastName" type={'text'} />
             </section>
             <section className="form-group">
-                <label htmlFor="address">Address</label>
-                <input className="form-control" name="address" type={'text'} />
+                <label htmlFor="address">Amount</label>
+                <input placeholder="20.00" className="form-control" name="address" type={'number'} />
             </section>
             <div className="cardInputContainer">
-                <CardElement className="cardInput" />
+                <CardElement className="cardInput" options={{
+                    style: {
+                        base: inputStyle,
+                    }
+                }} />
             </div>
             <Button title='Donate Now' />
         </form>
