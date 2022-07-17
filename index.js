5ed8b41719f72f0ceea830967d553f7f9b260ef2
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-07-16 21:47:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import './index.css';
 import App from './App';
 import { stripePromise, options } from './utils/stripe/stripe.utils';
 // import reportWebVitals from './reportWebVitals';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
